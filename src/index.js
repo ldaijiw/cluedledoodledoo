a914c649bdbab43141f04018b263962d6fdcf03f
OmerBaddour <omer.m.baddour@gmail.com>
2022-06-25 11:14:08 -0400

    basic wordle state with no actions

--- src/index.js
+++ src/index.js
@@ -1,17 +1,81 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { times } from 'lodash'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const LEN_WORDS = 5;
+const NUM_ROWS = 6;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Tile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      char: props.char,
+    };
+  }
+
+  render() {
+    return (
+      <button className="tile">
+        {this.state.char}
+      </button>
+    )
+  }
+}
+
+class Row extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      guess: props.guess,
+    };
+  } 
+
+  
+  render() {
+    return (
+      <div className="row">
+        {times(LEN_WORDS, i => 
+          <Tile key={i} char={this.state.guess?.[i]}/>
+        )}
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      // TODO randomize initialization of answer
+      answer: 'salad',
+      // TODO remove test array
+      // guesses: Array(NUM_ROWS).fill(null),
+      guesses: ["abcde", "fghij", "klmno", "pqrst", "uvwxy", "zabcd"]
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <div>
+          {times(NUM_ROWS, i =>
+            <Row key={i} guess={this.state.guesses[i]}/>
+          )}
+        </div>
+        <div className="submit-guess-button">
+          <button className="submit-guess">
+            Submit guess
+          </button>
+        </div>
+      </>
+    )
+  }
+}
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
+  
