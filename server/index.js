00d6850d6528b33f423c968bfefe4e7af55ac2b6
OmerBaddour <omer.m.baddour@gmail.com>
2022-08-10 20:55:10 +0100

    refactor: removed server APIs for guess and answer lists

--- server/index.js
+++ server/index.js
@@ -6,26 +6,8 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-const VALID_ANSWERS = fs
-  .readFileSync(path.resolve(__dirname, "data/answers.txt"))
-  .toString()
-  .split("\n");
-
-const VALID_GUESSES = fs
-  .readFileSync(path.resolve(__dirname, "data/guesses.txt"))
-  .toString()
-  .split("\n");
-
 // serve the files for the React app
 app.use(express.static(path.resolve(__dirname, "../client/build")));
-
-app.get("/validAnswers", (_req, res) => {
-  res.json({ validAnswers: VALID_ANSWERS });
-});
-
-app.get("/validGuesses", (_req, res) => {
-  res.json({ validGuesses: VALID_GUESSES });
-});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
