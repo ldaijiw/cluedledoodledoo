61a1de8d65ffc80863f241ef90635a0af594bc1c
OmerBaddour <omer.m.baddour@gmail.com>
2022-08-29 22:57:28 -0400

    style: added colors for day of week for daily button

--- client/src/data/constants.js
+++ client/src/data/constants.js
@@ -5,3 +5,13 @@ export const COLORS = {
   YELLOW: "#EBD021",
   GREEN: "#090",
 };
+// using Date.getDay() to return an integer [0,6], where 0 corresponds to Sunday
+export const COLORS_FOR_DAYS_OF_WEEK = [
+  "#F2B443",
+  "#FF5733",
+  "#33FFD5",
+  "#A5FF33",
+  "#33B4FF",
+  "#FF33FF",
+  "#AE33FF",
+];
