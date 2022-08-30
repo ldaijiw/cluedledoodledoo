37734c8591bf687f524b7901b369a8965d4ac520
OmerBaddour <omer.m.baddour@gmail.com>
2022-08-29 23:49:11 -0400

    fix: char color algorithm had a bug

--- client/src/components/row.js
+++ client/src/components/row.js
@@ -13,16 +13,22 @@ class Row extends React.Component {
       let answerCharCounts = { ...this.props.answerCharCounts };
 
       // assign colors to each char in guess
+      // find green letters
+      // NOTE important that finding green letters is done first in its entirety, otherwise (INFIX, XXXXX), will give first X yellow instead of last X green
+      this.props.guess.split("").forEach((char, index) => {
+        if (this.props.answer[index] === char) {
+          tileColors[index] = COLORS.GREEN;
+          answerCharCounts[char] -= 1;
+        }
+      });
+
+      // find yellow letters
       this.props.guess.split("").forEach((char, index) => {
         if (char in answerCharCounts && answerCharCounts[char] > 0) {
-          if (this.props.answer[index] === char) {
-            tileColors[index] = COLORS.GREEN;
-          } else {
+          if (tileColors[index] !== COLORS.GREEN) {
             tileColors[index] = COLORS.YELLOW;
+            answerCharCounts[char] -= 1;
           }
-
-          // update char count
-          answerCharCounts[char] -= 1;
         }
       });
     }
